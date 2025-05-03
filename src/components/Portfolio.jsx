3f72b920c396b8b55c9245f4fd072ff3922a1076
Adam Hammouche <ahammouche@hotmail.com>
2025-05-03 17:41:43 +0200

    last-commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -16,6 +16,10 @@ function Portfolio({ activeSection, handleClick }) {
       return project.type === 'site_vitrine'; // Afficher uniquement les projets de type "site_vitrine"
     } else if (activeFilter === 'site_dynamique') {
       return project.type === 'site_dynamique'; // Afficher uniquement les projets de type "site_dynamique"
+    } else if (activeFilter === 'site_wordpress') {
+      return project.type === 'site_wordpress'; // Afficher uniquement les projets de type "site_wordpress"
+    } else if (activeFilter === 'site_fullstack') {
+      return project.type === 'site_fullstack'; // Afficher uniquement les projets de type "site_fullstack"
     }
     return false;
   });
@@ -26,24 +30,40 @@ function Portfolio({ activeSection, handleClick }) {
           <h2>Mon <span>Portfolio</span></h2>
         </div>
         <div className="button-box">
-          <button
-            className={activeFilter === 'all' ? 'button active' : 'button'}
-            onClick={() => setActiveFilter('all')}
-          >
-            Tous
-          </button>
-          <button
-            className={activeFilter === 'site_vitrine' ? 'button active' : 'button'}
-            onClick={() => setActiveFilter('site_vitrine')}
-          >
-            Sites Vitrines
-          </button>
-          <button
-            className={activeFilter === 'site_dynamique' ? 'button active' : 'button'}
-            onClick={() => setActiveFilter('site_dynamique')}
-          >
-            Sites Dynamiques
-          </button>
+          <div className='box-1' >
+            <button
+              className={activeFilter === 'all' ? 'button active' : 'button'}
+              onClick={() => setActiveFilter('all')}
+            >
+              Tous
+            </button>
+            <button
+              className={activeFilter === 'site_vitrine' ? 'button active' : 'button'}
+              onClick={() => setActiveFilter('site_vitrine')}
+            >
+              Sites Vitrines
+            </button>
+            {/* <button
+              className={activeFilter === 'site_dynamique' ? 'button active' : 'button'}
+              onClick={() => setActiveFilter('site_dynamique')}
+            >
+              Sites Dynamiques
+            </button> */}
+          </div>
+          <div className='box-1'>
+            <button
+              className={activeFilter === 'site_wordpress' ? 'button active' : 'button'}
+              onClick={() => setActiveFilter('site_wordpress')}
+            >
+              Sites Wordpress
+            </button>
+            <button
+              className={activeFilter === 'site_fullstack' ? 'button active' : 'button'}
+              onClick={() => setActiveFilter('site_fullstack')}
+            >
+              Sites Fullstack
+            </button>
+          </div>
         </div>
       </div>
       <div className="site-cards-container">
--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 const projects = [
     {
-        id: 1, 
-        title: 'Booki', 
-        description: 
-        `Création de la page d'accueil,
+        id: 1,
+        title: 'Booki',
+        description:
+            `Création de la page d'accueil,
         Intégration du contenu conformément à une maquette,
         Implémentation d'une interface responsive.
-        Technologies: HTML/CSS `, 
-        image: '/portfolio1.jpg', 
+        Technologies: HTML/CSS `,
+        image: '/portfolio1.jpg',
         type: 'site_vitrine',
         url: 'https://adam0794hh.github.io/Hammouche_Adam_2_code_112022/'
     },
@@ -20,37 +20,48 @@ const projects = [
         Technologies: HTML/CSS/Git et Github`,
         image: '/portfolio2.jpg',
         type: 'site_vitrine',
-        url: 'https://ohhmyfoood.netlify.app' 
+        url: 'https://ohhmyfoood.netlify.app'
     },
     {
         id: 3,
-        title: 'Nina Carducci',
-        description: `Optimisez la performance et le référencement d'un site de photographe,
-        Technologies: HTML/CSS/JavaScript.`,
-        image: '/portfolio3.jpg',
-        type: 'site_dynamique',
-        url:'https://adam0794hh.github.io/ninacarducci.github.io-main/' 
+        title: 'Kardoune Cosmetique',
+        description: `Création du site e-commerce Kardoune Cosmétique sous WordPress. 
+        Intégration WooCommerce et design personnalisé avec Elementor. 
+        Techno : WordPress, Elementor, WooCommerce.`,
+        image: '/portfolio6.jpg',
+        type: 'site_wordpress',
+        url: 'https://www.kardounecosmetique.fr/'
     },
     {
         id: 4,
-        title: 'Kasa',
-        description: 
-        `Initialiser Configurer Développer une application avec React,
-        Technologies: HTML/CSS/React.`,
-        image: '/portfolio4.jpg',
-        type: 'site_dynamique',
-        url: 'https://oc-project-kasa.netlify.app/' 
+        title: 'Glowy Agency',
+        description: `Conception du site e-commerce Glowy Agency sous WordPress avec Elementor. 
+        Mise en place de la boutique en ligne et d’un design épuré. 
+        Techno : WordPress, Elementor, WooCommerce.`,
+        image: '/portfolio3.jpg',
+        type: 'site_wordpress',
+        url: 'https://www.glowy-agency.fr/'
     },
     {
         id: 5,
-        title: 'Print it',
-        description: `Dynamiser et Programmer le site Internet Print it,
-        en utilisant les fondamentaux de JavaScript,
-        Technologies: HTML/CSS/JavaScript`,
-        image: '/portfolio5.jpg',
-        type: 'site_dynamique',
-        url: 'https://oc-project-printit.netlify.app/'
+        title: 'Kasa',
+        description:
+            `Initialiser Configurer Développer une application avec React,
+        Technologies: HTML/CSS/React.`,
+        image: '/portfolio4.jpg',
+        type: 'site_vitrine',
+        url: 'https://oc-project-kasa.netlify.app/'
     },
-    
-  ];
+    {
+        id: 6,
+        title: 'Tresor de Musc',
+        description: `Création du site e-commerce Trésor de Musc avec WordPress et Elementor. 
+        Mise en place d'une boutique responsive, optimisée UX/UI. 
+        Techno : WordPress, Elementor, WooCommerce.`,
+        image: '/portfolio5.jpg',
+        type: 'site_wordpress',
+        url: 'https://www.tresordemusc.fr/'
+    },
+
+];
 export default projects;
